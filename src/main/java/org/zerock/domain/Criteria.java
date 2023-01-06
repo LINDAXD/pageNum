0e3ffc15c730c823eee83b4a44564d79e2f77058
LINDAXD <dyang420@naver.com>
2023-01-06 16:34:00 +0900

    게시판 p324

---
+++ src/main/java/org/zerock/domain/Criteria.java
@@ -0,0 +1,23 @@
+package org.zerock.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+public class Criteria {
+	
+	private int pageNum;
+	private int amount;
+	
+	public Criteria() {
+		this(1,10);
+	}
+
+	public Criteria(int pageNum, int amount) {
+		super();
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+
+}
